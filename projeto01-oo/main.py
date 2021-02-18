51a1428a9d8837ce02f1b5819de24920854a5c88
Dario Campagna <campagnacoutinho67@gmail.com>
2021-02-18 20:59:39 -0300

    Atributos privados.

--- projeto01-oo/main.py
+++ projeto01-oo/main.py
@@ -3,13 +3,3 @@ from conta import Conta
 conta1 = Conta(1, "Fulano", 10000.0, 1000.0)
 conta2 = Conta(2, "Beltrano", 0.0, 1000.0)
 conta3 = Conta(3, "Sicrano", 0.0, 2000.0)
-
-print(conta1)
-
-print(conta1.extrato())
-
-print(conta1.saca(200))
-
-print(conta1.deposita(200))
-
-print(conta1.extrato())

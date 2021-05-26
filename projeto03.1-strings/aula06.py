1babe98196b57e2956b1eebeffe2daf301a9d4db
Dario Campagna <campagnacoutinho67@gmail.com>
2021-05-25 21:07:00 -0300

    Aula 06 - Strings.

---
+++ projeto03.1-strings/aula06.py
@@ -0,0 +1,30 @@
+#url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+
+url = ""
+
+#Tratando a URL
+url = url.strip()
+
+#Validação url
+if url == "":
+    raise ValueError('A URL está vazia!!')
+
+#Separa a base dos parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+print(url_base)
+url_parametros = url[indice_interrogacao + 1:]
+print(url_parametros)
+
+
+#Busca o valor de um parâmetro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+print(indice_parametro)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
